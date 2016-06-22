39cb5f747bd0b64fa4d33f3cae460087b311e3c2
Jason Stillerman <jason.t.stillerman@gmail.com>
2016-06-22 15:11:16 -0400

    Added Option to choose between VPC and Classic

--- spec/lib/questionsSpec.js
+++ spec/lib/questionsSpec.js
@@ -2,8 +2,8 @@ var questions = require('../../lib/questions.js');
 
 describe('questions.js', function() {
   describe('format', function() {
-    it('should be an array of 25 questions', function() {
-      expect(questions.length).toEqual(26);
+    it('should be an array of 27 questions', function() {
+      expect(questions.length).toEqual(27);
     });
   });
 });
