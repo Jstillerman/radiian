54558591a7b4f8d13e0cd07f45bf64fb9f705260
Jason Stillerman <jason.t.stillerman@gmail.com>
2016-06-22 13:45:33 -0400

    Added tests for VPC

--- spec/lib/questionsSpec.js
+++ spec/lib/questionsSpec.js
@@ -3,7 +3,7 @@ var questions = require('../../lib/questions.js');
 describe('questions.js', function() {
   describe('format', function() {
     it('should be an array of 25 questions', function() {
-      expect(questions.length).toEqual(25);
+      expect(questions.length).toEqual(26);
     });
   });
 });
--- spec/lib/regexesSpec.js
+++ spec/lib/regexesSpec.js
@@ -12,6 +12,16 @@ describe('regexes.js', function() {
     });
   });
 
+  describe('vpc_subnet_id', function(){
+    it("should fail on anything that doesnt start with vpc", function(){
+      expect(regexes.vpc.test('ami-47a23a30')).toBeFalsy();
+    });
+
+    it("should fail on anything that doesnt end with 8 alphanumeric chars", function(){
+      expect(regexes.vpc.test('vpc-47a2330')).toBeFalsy();
+    });
+  });
+
   describe('awsKey', function() {
     // TODO
   });
